425985fad20f1b7daecf01d606433c741c6b2891
raisha.vysyaraju <raisha.vysyaraju@encora.com>
2023-04-26 11:56:49 +0530

    Display Date Assignment

---
+++ DateExample/src/com/encora/util/Entry.java
@@ -0,0 +1,11 @@
+package com.encora.util;
+
+public class Entry {
+
+	public static void main(String[] args)
+{
+		MyDate d = new MyDate();
+		
+		
+}
+	}
---
+++ Training Session/src/Datetest/Entry.java
@@ -0,0 +1,11 @@
+package Datetest;
+
+public class Entry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			DateDisplay dd = new DateDisplay(-24,-7,1988);
+			System.out.println("display Date is:"+dd);
+	}
+
+}
